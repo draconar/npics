27ee3eb3ace396689f984edfd4748361151fc31c
Fabio Fonseca <draco@linux-g351.site>
2013-08-23 07:01:54 -0300

    RC1

--- server.js
+++ server.js
@@ -1,96 +1,51 @@
 
-var express = require('express');
+var express = require('express')
+  , app = express()
+  , cons = require('consolidate')
+  , io = require('socket.io').listen(app.listen(process.env.PORT))
+  , fs = require('fs'), twitter = require('ntwitter')
+  , util = require('util');
 
-var app = express();
 
-var url = require('url');
 
-var oauth = require('oauth');
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/');
+app.use(express.static(__dirname + '/'));
 
-app.get('/', function(request, response) {
-	  response.send('Hello, world.');
+var twit = new twitter({
+  consumer_key:     process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret:  process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
-
-app.get('/tweets.json', function(request, response) {
-	  var query = {
-		   'q': '',
-		   'geocode':'37.781157,-122.398720,5km',
-		   'count': '20'
-	  };
-
-	    var count = parseInt(request.param('count'));
-
-	      if (!isNaN(count)) {
-		          query.count = count;
-			    }
-
-	        var twitter = new oauth.OAuth(
-			    'https://api.twitter.com/oauth/request_token',
-			        'https://api.twitter.com/oauth/access_token',
-				    process.env.TWITTER_CONSUMER_KEY,
-				        process.env.TWITTER_CONSUMER_SECRET,
-					    '1.0A',
-					        null,
-						    'HMAC-SHA1'
-						      );
-
-		  twitter.get(
-				      url.format({
-					            protocol: 'https:',
-					            hostname: 'api.twitter.com',
-					            pathname: '/1.1/search/tweets.json',
-					            query: query
-					          }),
-				          process.env.TWITTER_ACCESS_TOKEN,
-					      process.env.TWITTER_ACCESS_TOKEN_SECRET,
-					          function(err, data) {
-							        if (err) {
-									        response.jsonp(err);
-										      } else {
-											              var tweets = [];
-/*if(data) {
-	JSON.parse(data).forEach(function(tweet) {
-														                tweets.push({
-																	            'id_str': tweet.id_str,
-																	            'created_at': tweet.created_at,
-																	            'text': tweet.text
-																	          });
-																        });
-}*/
-
-
-
-													              response.jsonp({
-															                'statusCode': 200,
-															                'data': JSON.parse(data)
-															              });
-														            }
-								    }
-		    );
-});
-
-app.listen(process.env.PORT || 3000);
-
-/*
-var express = require('express');
-var app = express();
-
-app.configure(function(){
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.engine('ejs', require('ejs').__express);
-app.set('views', __dirname + '/app');
-
-app.use(express.static(__dirname + '/app'));
-});
-
 
 app.get('/', function(req, res){
-  res.render('index.ejs', {
-    appID : '495633883851289'
+  res.render('index', {
+    title: 'Consolidate.js'
   });
 });
 
-app.listen(process.env.PORT || 3000);
-*/
+// Heroku won't actually allow us to use WebSockets
+// // so we have to setup polling instead.
+// // https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+io.configure(function () {
+   io.set("transports", ["xhr-polling"]);
+   io.set("polling duration", 10);
+});
+
+io.sockets.on('connection', function(socket) {
+	socket.on('message', function (message) {
+		console.log('location received');
+		var loc = message;
+		//'-53.525391,-22.762886,-43.472900,-20.363126'
+        twit.stream('statuses/filter', {'locations':loc},
+			function(stream) {
+			  stream.on('data',function(data){
+				socket.emit('twitter',data);
+			  });
+		});   
+    });
+  
+});
+
